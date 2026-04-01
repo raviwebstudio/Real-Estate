4d01a603fcdcd1459852124822dac62defbffcea
Ravi Kumar Gautam <ravi.g.live@gmail.com>
2026-04-01 12:28:42 +0530

    new update

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   output: "standalone",
   poweredByHeader: false,
   compress: true,
+  allowedDevOrigins: ["192.168.1.60"],
   images: {
     formats: ["image/avif", "image/webp"],
   },
